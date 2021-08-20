1570af6e1fc1d8c18e66424f23821af3249765a4
LakinduK <lakindu1997@gmail.com>
2021-08-20 07:17:47 +0530

    dashboard table update

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -24,7 +24,8 @@ $result = mysqli_query($conn, $sql);
     <title>KRISH VILLA - Admin Dashboard</title>
 
     <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="../assets/fonts/font-awesome/font-awesome.css">
+    <!-- <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css"> -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
