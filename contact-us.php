20f49d79b4ae9911e54c6f9abe19f09a5134ec57
LakinduK <lakindu1997@gmail.com>
2021-08-20 07:03:05 +0530

    added admin page and content

--- contact-us.php
+++ contact-us.php
@@ -69,6 +69,7 @@ include 'includes/JS.php';
     </div>
 
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.97915747782!2d107.58270291427688!3d-6.893096195019089!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e67b57d420db%3A0x4dd071fcb9157e80!2sBTC+Fashion+Mall!5e0!3m2!1sen!2sid!4v1522964715022" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen></iframe>
+
 </div>
 </div>
 
